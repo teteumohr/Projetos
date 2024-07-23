66ba882cf9abc362777cf8487d571e10341fd04a
Matheus Mohr <teteumohrbr@hotmail.com>
2024-07-22 21:43:12 -0300

    Adicioando a interface com os metodos abstratos

---
+++ src/Projetos/AnimalPolimorfismo/AnimalMetodos.java
@@ -0,0 +1,7 @@
+package Projetos.AnimalPolimorfismo;
+
+public abstract interface AnimalMetodos {
+    public abstract void locomover();
+    public abstract void alimentar();
+    public abstract void emitirSom();
+}
